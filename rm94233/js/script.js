11d8ba37cd1b9a49c1cd3398aad9e588a596efe0
LucasKenji01 <rm94233@fiap.com.br>
2022-08-03 20:58:30 -0300

    Elementos

---
+++ rm94233/js/script.js
@@ -0,0 +1,37 @@
+
+//console.log() - O console dá saída em informações no console do navegador!
+console.log("Alô Mundo!")
+
+//document - O document manipula elementos do DOM
+//document.write("Olá Mundo!")
+
+const btn = document.getElementById("btnEnviar")
+
+//function msg(){
+//  alert('CLIQUEI')
+//}
+
+btn.addEventListener("click",()=>{
+    //alert(`Resultado : ${minhaFuncao()}` )
+   //let inputTask = document.querySelector("input[name=descTask]") 
+   let inputTask = document.querySelectorAll("input")
+
+    inputTask[2].value = btn.innerText
+
+})
+
+
+//ARROW FUNCTION
+const minhaFuncao = () => {
+    let a = 10
+    let b = 10
+    return(a + b + c)
+}
+
+//HOISTING
+var c = 5
+
+
+//CRIAR UM OBJETO
+//ADICIONAR OS DADOS DOS CAMPOS NO OBJETO
+//IMPRIMIR O OBJETO COM console.log()
