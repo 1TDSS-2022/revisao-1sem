d66010230dcfb0b0bbc4a6145851426174dba5bd
RodrigoMachadoC <rm94301@fiap.com.br>
2022-08-04 20:42:28 -0300

    add script.js

--- m94301/js/script.js
+++ m94301/js/script.js
@@ -1,31 +1,29 @@
+//console.log() - O console da saída em informações no console do navegador!
+console.log("Olá Mundo!")
 
-//console.log() - o console dá saida em informações no console do navegador!
-//console.log("Alô Mundo!")
-
-//document - O documet manipula elementos do DOM
-//document.write("Olá Mundo!")
+//document - O document manipula elementos do DOM
+//document.write("Alô Mundo!") Obs. se estiver com o defer no script, não funciona.
 
 const btn = document.getElementById("btnEnviar")
 
-//function msg(){
-//    alert('CLIQUEI')
-//}
-
-btn.addEventListener("click",()=>{
-    alert('Resultado: ${minhaFuncao()}' )
-    Let inputTask = document.querySelectorAll("input") 
+btn.addEventListener("click", ()=>{
+    //alert(`Resultado: ${minhaFuncao()}`)
+    //let inputTask = document.querySelector("input[name=descTask]")
+    let inputTask = document.querySelectorAll("input")
 
     inputTask[2].value = btn.innerText
-    
 })
 
-const minhaFuncao =() => {
-    Let a = 10
-    Let b = 10
+//ARROW FUNCTION
+const minhaFuncao = ()=> {
+    let a = 10
+    let b = 10
     return (a + b + c)
 }
 
-
-
-
+//HOISTING
 var c = 5
+
+//CRIAR UM OBJETO
+//ADICIONAR OS DADOS DOS CAMPOS NO OBJETO
+//IMPRIMIR O OBJETO COM console.log()
