b7528713a0f595189b0aee32d1570b541e23d5b5
FilipeJava <phill.java@gmail.com>
2022-08-03 21:00:03 -0300

    criado scripit

---
+++ RM93042/js/script.js
@@ -0,0 +1,49 @@
+
+// no painel do desenvolvedor
+console.log("Começamos a Revisão JS")
+
+// document.write("Escrevendo com Write") deve usar sem o defer ... procura o primeiro espaço para escrever diretamente no html
+
+
+const botao = document.getElementById("btnEnviar")
+
+botao.addEventListener("click",()=>{
+//    let inputTask = document.querySelectorAll("input[name=descTask]")
+let inputTask = document.querySelectorAll()
+
+inputTask[2].value=botao.innerText
+
+
+})
+
+
+//tarefa 
+//Criar Objeto
+//adicionar os dados cmpos no objeto
+// printo no console.log
+
+
+
+
+
+// botao.addEventListener("click",()=>{
+//     alert(minhaFuction())
+// })
+
+// modelo de como criar uma funcao // realizar dessa forma
+
+// const minhaFuction = ()=>{
+//     let a = 10
+
+//     let b = 10
+
+//     return (a+b)
+// }
+
+
+
+
+
+
+
+
