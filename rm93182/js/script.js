153e7f427908bc3305879e9959aa80d2d3d33141
PedroSanagiotto <rm93182@fiap.com.br>
2022-08-03 21:01:21 -0300

    modificaçao

---
+++ rm93182/js/script.js
@@ -0,0 +1,36 @@
+
+//console.log() - O console dá saída em informações no console do navegador!
+//console.log("Alô mundo")
+
+//document - O documento manipula elementos do DOM - somente se tirar o defer
+//(o defer deixa a ação do document write em stand by)
+//document.write("Olá Mundo")
+
+const btn = document.getElementById("btnEnviar")
+
+//function msg(){
+//   alert('resultado : ')
+// }
+
+btn.addEventListener("click",()=>{
+    //alert('Resultado : ${minhaFuncao()}' )
+    //let inputTask = document.querySelector("input[nam=descTask]")
+    let inputTask = document.querySelectorAll("input")
+
+    inputTask[2]
+
+
+})
+
+//arow function
+const minhaFuncao = () => {
+    let a = 10
+    let b = 10
+    return(a + b)
+}
+//HOISTING
+var c = 5
+
+//CRIAR UM OBJETO
+//ADICIONAR OS DADOS DOS CAMPOS DO OBJETO
+//IMPRIMIR O OBJETO COM console.log()
