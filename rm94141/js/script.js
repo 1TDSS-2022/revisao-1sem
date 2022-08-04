e03c4874326e3d22680b5e741702c8f6ef834f26
mariananieton <rm94141@fiap.com.br>
2022-08-03 21:10:33 -0300

    alterando o "let"

---
+++ rm94141/js/script.js
@@ -0,0 +1,31 @@
+
+//console.log() - o console dá saida em informações no console do navegador!
+//console.log("Alô Mundo!")
+
+//document - O documet manipula elementos do DOM
+//document.write("Olá Mundo!")
+
+const btn = document.getElementById("btnEnviar")
+
+//function msg(){
+//    alert('CLIQUEI')
+//}
+
+btn.addEventListener("click",()=>{
+    alert('Resultado: ${minhaFuncao()}' )
+    let inputTask = document.querySelectorAll("input") 
+
+    inputTask[2].value = btn.innerText
+    
+})
+
+const minhaFuncao =() => {
+    let a = 10
+    let b = 10
+    return (a + b + c)
+}
+
+
+
+
+var c = 5
