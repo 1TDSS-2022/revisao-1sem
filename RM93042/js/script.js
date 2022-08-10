8b0fa5a5c2a58323a1076cf69cd34ef3f6df215d
FilipeJava <RM93042@fiap.com.br>
2022-08-10 12:28:20 -0300

    script finalizado

--- RM93042/js/script.js
+++ RM93042/js/script.js
@@ -1,20 +1,69 @@
+// pratica
+
+
+// Criando o objeto tarefa
+const tarefa ={
+    nmTask:'',      // atributos referentes aos campos inputs
+    descTask:'',
+    dtTask:'',
+        task:function(nmTask,descTask,dtTask){ // metodo construtor para atribuir os parametros ao atributo
+            this.nmTask     = nmTask
+            this.descTask   =descTask
+            this.dtTask     =dtTask
+        }
+}
+
+
+// recuperar o botao
+const btn = document.getElementById("btnEnviar")
+
+//Atrelar evento de click ao botao add a tarefa popular o objeto
+
+btn.addEventListener('click',function(){
+        // Recuperando os dados do form
+    let inputTask= document.querySelectorAll('input[type=text],input[type=date]')
+    tarefa.task(inputTask[0].value,inputTask[1].value,inputTask[2].value)
+    console.log(tarefa)
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // no painel do desenvolvedor
-console.log("Começamos a Revisão JS")
+// console.log("Começamos a Revisão JS")
 
 // document.write("Escrevendo com Write") deve usar sem o defer ... procura o primeiro espaço para escrever diretamente no html
 
 
-const botao = document.getElementById("btnEnviar")
+// const botao = document.getElementById("btnEnviar")
 
-botao.addEventListener("click",()=>{
-//    let inputTask = document.querySelectorAll("input[name=descTask]")
-let inputTask = document.querySelectorAll()
+// botao.addEventListener("click",()=>{
+// //    let inputTask = document.querySelectorAll("input[name=descTask]")
+// let inputTask = document.querySelectorAll()
 
-inputTask[2].value=botao.innerText
+// inputTask[2].value=botao.innerText
 
 
-})
+// })
 
 
 //tarefa 
